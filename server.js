e0476e187bf73099c72ab98391143b9a41440a39
marvellouse683-prog <marvellousediagbonya683@gmail.com>
2025-09-20 19:02:44 +0100

    Initial commit with contact form functionality contact form now sends data to server and handles responses correctly. Added contact.js route to handle form submissions. Updated server.js to serve static files and use contact route. Updated public/script.js to handle form submission and display response messages. Updated package.json and package-lock.json with necessary dependencies. Updated public/index.html with contact form structure. Updated public/style.css for basic styling of the contact form.

--- server.js
+++ server.js
@@ -1,19 +1,25 @@
+require("dotenv").config();
 // Create express 
+
  const express = require("express")
 //  create express application
 const app = express();
+const bodyParser = require("body-parser");
+const contactRouter = require('./routes/contact')
  
 // create path
 const path = require("path");
 //initialize  port
 const port = 8080
-const oneYear = 31536000;
-// use express app to serve static
-app.use(express.static(path.join(__dirname, "public"), {
-    maxAge: oneYear * 1000
-}));
 
+//use middleware to parse request body
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+// use express app to serve static
+app.use(express.static(path.join(__dirname, "public"), {}));
+
+//use contact form route for submit route
+app.use('/submitForm', contactRouter);
 // Listen to port
 app.listen(port, () => {
-     console.log(`Server is running on https://127.0.0.1:${port}`)
- })
+    console.log(`Server is running on https://127.0.0.1:${port}`)});
